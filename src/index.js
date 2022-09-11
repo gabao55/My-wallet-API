425ada93257a4a62f503b19ab3e0e68c874e0abd
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-09-11 11:01:46 -0300

    Implements routers pattern to manage the routes of the application

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import db from './database/db.js';
-import { signIn, signUp } from './controllers/auth.controllers.js';
-import { createTransaction, getUserTransactions } from './controllers/transactions.controller.js';
+import authRouter from './routers/auth.router.js';
+import transactionsRouter from './routers/transactions.router.js';
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.post('/auth/sign-up', signUp);
-server.post('/auth/sign-in', signIn);
-
-server.post('/transactions', createTransaction);
-server.get('/transactions', getUserTransactions);
-
-// Testing part (delete after finishing project)
-
-server.get('/sessions', async (req, res) => {
-    const sessions = await db.collection('sessions').find({}).toArray();
-
-    res.send(sessions);
-});
-
-server.get('/users', async (req, res) => {
-    const users = await db.collection('users').find({}).toArray();
-
-    res.send(users);
-});
+server.use(authRouter);
+server.use(transactionsRouter);
 
 server.listen(5000, () => console.log("Listening to PORT 5000"));
